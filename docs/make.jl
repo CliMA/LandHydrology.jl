5f7440c84efe8cc619b8d59e4c6e23be5d599eb9
kmdeck <kdeck@caltech.edu>
2021-09-02 15:09:47 -0700

    fixed typo

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using LandHydrology
 
 makedocs(
     sitename = "LandHydrology.jl",
-    authors = "CliMA Land Model Team"
+    authors = "CliMA Land Model Team",
     format = Documenter.HTML(collapselevel = 1, mathengine = MathJax3()),
     pages = [
     "Home" => "index.md",
