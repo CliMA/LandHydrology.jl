cf62b39265f870a538a19c3779c469e9f9a4acf2
kmdeck <kdeck@caltech.edu>
2021-09-07 16:11:54 -0700

    adding docs
    added in md
    
    fixed formatter, maybe, deployed docs link
    
    formatting
    
    formatting
    
    cant find docs
    
    bug
    
    docs deploy link issue
    
    still trying to fix docs
    
    still trying to fix docs2
    
    fixing docs
    
    docs
    
    fixed column error
    
    added in Dirichlet BC; updated tests; wip
    
    type issue
    
    type issue
    
    updated BC; heat test still has bug
    
    found error
    
    didnt need to export so much
    
    tests run
    
    small changes
    
    boundary labels are in domain
    
    heat passes locally
    
    addressed looping question
    
    added doc strings
    
    add DocStringExtensions
    
    error in method args

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,24 @@
-include("SoilModel/richards_equation.jl")
+using ClimaCore: Fields
+using CLIMAParameters
+struct EarthParameterSet <: AbstractEarthParameterSet end
+const param_set = EarthParameterSet()
+
+using OrdinaryDiffEq:
+    ODEProblem,
+    solve,
+    CarpenterKennedy2N54,# does not work
+    SSPRK33,
+    SSPRK73
+using LandHydrology
+using LandHydrology.Domains: Column, make_function_space
+using LandHydrology.SoilInterface
+using LandHydrology.SoilInterface.SoilWaterParameterizations
+using LandHydrology.SoilInterface.SoilHeatParameterizations
+using Statistics
+using Test
+using ArtifactWrappers
+using DelimitedFiles
+
 include("test_domains.jl")
-#Currently these tests dont use the new interface b/c dirichlet BC dont work yet.
-include("SoilModel/richards_vg.jl")
-include("SoilModel/heat_analytic_unit_test.jl")
+include("SoilModel/richards_equation.jl")
+include("SoilModel/heat_test_interface.jl")
