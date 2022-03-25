9c2779e7b49568f7e2359e9b50f38a3c5c0e9ac9
Anna GL <gagnelandmanna@gmail.com>
2022-03-24 21:38:40 -0700

    3 roots3 roots3 roots

--- src/plant_hydraulics2.0.jl
+++ src/plant_hydraulics2.0.jl
@@ -53,7 +53,7 @@ function flow(z_do, z_up, p_do, p_up, a, b, Kmax, a_up, b_up, Kmax_up)
     return flow
 end
 
-#=
+
 function theta_to_p(theta)      
     p = (theta.-1).*5;  
     return p
@@ -63,7 +63,8 @@ function p_to_theta(p)
     theta = p./5 .+ 1; 
     return theta
 end
-=#
+
+#=
 
 function theta_to_p(theta, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s) 
     if theta > theta_ft      
@@ -94,6 +95,7 @@ function p_to_theta(p, pi_0, pi_tlp, theta_r, theta_ft, epsilon, S_s)
     end 
     return theta  
 end
+=#
 
 function fstem!(F_stem,x)
     flow_in_stem = flow(z_soil, z_bottom_stem, p_soil, x[1], a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles) 
@@ -107,13 +109,19 @@ end
 
 # Solver
 function roots(dy,y,paramset,t)
-    #p_stem = theta_to_p(y[1]/size_reservoir_stem_moles) 
-    #p_leaf = theta_to_p(y[2]/size_reservoir_leaf_moles) 
+    p_stem = theta_to_p(y[1]/size_reservoir_stem_moles) 
+    p_leaf = theta_to_p(y[2]/size_reservoir_leaf_moles) 
 
-    p_stem = theta_to_p(y[1]/size_reservoir_stem_moles, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
-    p_leaf = theta_to_p(y[2]/size_reservoir_leaf_moles, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
+    #p_stem = theta_to_p(y[1]/size_reservoir_stem_moles, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
+    #p_leaf = theta_to_p(y[2]/size_reservoir_leaf_moles, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
 
-    flow_in_stem = flow(z_soil, z_bottom_stem, p_soil, p_stem, a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles)
+    flow_in_stem = ones(n_roots,1)
+    for i in 1:n_roots
+        flow_in_stem[i] = flow(z_soil[i], z_bottom_stem, p_soil[i], p_stem, a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles)
+        #@show(flow_in_stem)
+    end
+
+    #flow_in_stem = flow(z_soil, z_bottom_stem, p_soil, p_stem, a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles)
     flow_out_stem = flow(z_bottom_stem, z_leaf, p_stem, p_leaf, a_stem, b_stem, K_max_stem_moles, a_leaf, b_leaf, K_max_leaf_moles)
     
     if t < 500 
@@ -124,28 +132,32 @@ function roots(dy,y,paramset,t)
         T = 10*(T_0/5)*500/500+T_0
     end
 
-    dy[1] = flow_in_stem - flow_out_stem
+    dy[1] = sum(flow_in_stem) - flow_out_stem
     dy[2] = flow_out_stem - T
 end
 
 # Initial values
 T_0 = 0.01/mass_mole_water # moles per s, using value at noon fig 9j) Christoffersen                    
-p_soil = -0.02 # MPa
-z_soil = 0.0 # m
-z_bottom_stem = h_root # m
+p_soil = [-0.1; -0.2; -0.3] # MPa want it to be wet and wetter than rest of plant, so close to 0
+z_soil = [0; 1; 2]
+n_roots = length(p_soil)
+
+#p_soil = -0.02 # MPa
+#z_soil = 0.0 # m
+z_bottom_stem = 5 #h_root # m
 z_leaf = h_stem # m
 
 # Set system to equilibrium state by setting LHS of both ODEs to 0
 #solnstem = nlsolve(fstem!, [-1.0])
 p_stem_ini = -0.5 #solnstem.zero[1]
 #solnleaf= nlsolve(fleaf!, [-1.0])
-p_leaf_ini = -0.5 #solnleaf.zero[1]
+p_leaf_ini = -0.4 #solnleaf.zero[1]
 
-#theta_stem_0 = p_to_theta(p_stem_ini) 
-#theta_leaf_0 = p_to_theta(p_leaf_ini) 
+theta_stem_0 = p_to_theta(p_stem_ini) 
+theta_leaf_0 = p_to_theta(p_leaf_ini) 
 
-theta_stem_0 = p_to_theta(p_stem_ini, pi_0, pi_tlp, theta_r, theta_ft, epsilon, S_s)
-theta_leaf_0 = p_to_theta(p_leaf_ini, pi_0, pi_tlp, theta_r, theta_ft, epsilon, S_s)
+#theta_stem_0 = p_to_theta(p_stem_ini, pi_0, pi_tlp, theta_r, theta_ft, epsilon, S_s)
+#theta_leaf_0 = p_to_theta(p_leaf_ini, pi_0, pi_tlp, theta_r, theta_ft, epsilon, S_s)
 
 y1_0 = float(theta_stem_0*size_reservoir_stem_moles)
 y2_0 = float(theta_leaf_0*size_reservoir_leaf_moles)
@@ -179,11 +191,11 @@ plot!(sol.t, y_theta_2, label="leaf", dpi=500)
 savefig("relative_water_content2.0.png") 
 
 # Compute pressure and flow rates from soln and plot
-#p_stem = theta_to_p(y_theta_1) 
-#p_leaf = theta_to_p(y_theta_2) 
+p_stem = theta_to_p(y_theta_1) 
+p_leaf = theta_to_p(y_theta_2) 
 
-p_stem = theta_to_p.(y_theta_1, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
-p_leaf = theta_to_p.(y_theta_2, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
+#p_stem = theta_to_p.(y_theta_1, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
+#p_leaf = theta_to_p.(y_theta_2, pi_0, theta_r, theta_tlp, theta_ft, epsilon, S_s)
 
 T = collect(0.0:dt:Float64(tend))
 tlength = 1:length(T)
@@ -205,9 +217,23 @@ plot!(sol.t,p_leaf_ini.*ones(length(sol.t),1),label="p_leaf_0",dpi=500)
 savefig("pressure2.0.png") 
 
 # Plot flow as a function of time [mol s-1]
-flow_in_stem = flow(z_soil, z_bottom_stem, p_soil, p_stem, a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles)
+flow_in_stem = ones(length(p_stem),n_roots)
+for j in 1:length(p_stem)
+    for i in 1:n_roots
+        flow_in_stem[j,i] = flow(z_soil[i], z_bottom_stem, p_soil[i], p_stem[j], a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles)
+    end
+end
+
+#flow_in_stem = flow(z_soil, z_bottom_stem, p_soil, p_stem, a_root, b_root, K_max_effective_root_moles, a_stem, b_stem, K_max_stem_moles)
+flow_in_stem_root_1 = flow_in_stem[:,1]
+flow_in_stem_root_2 = flow_in_stem[:,2]
+flow_in_stem_root_3 = flow_in_stem[:,3]
+total_flow_in_stem = sum(flow_in_stem, dims=2)
 flow_out_stem = flow(z_bottom_stem, z_leaf, p_stem, p_leaf, a_stem, b_stem, K_max_stem_moles, a_leaf, b_leaf, K_max_leaf_moles)
-plot(sol.t,flow_in_stem,linewidth=2,xaxis="time [s]",yaxis="flow [mol s-1]",label="flow into stem",legend=:bottomright,dpi=500)
+plot(sol.t,flow_in_stem_root_1,linewidth=2,xaxis="time [s]",yaxis="flow [mol s-1]",label="flow into stem from root 1",legend=:bottomright,dpi=500)
+plot!(sol.t,flow_in_stem_root_2,linewidth=2,xaxis="time [s]",yaxis="flow [mol s-1]",label="flow into stem from root 2",legend=:bottomright,dpi=500)
+plot!(sol.t,flow_in_stem_root_3,linewidth=2,xaxis="time [s]",yaxis="flow [mol s-1]",label="flow into stem from root 3",legend=:bottomright,dpi=500)
+plot!(sol.t,total_flow_in_stem,linewidth=2,xaxis="time [s]",yaxis="flow [mol s-1]",label="total flow into stem",legend=:bottomright,dpi=500)
 plot!(sol.t,flow_out_stem,linewidth=2,label="flow into leaves",dpi=500)
 plot!(sol.t,T,linewidth=2,label="transpiration boundary condition",dpi=500)
 savefig("flow2.0.png") 
